78c2459a2385f0e2e1420433e26036f72c73977b
Christopher Eeles <christopher.eeles@outlook.com>
2019-01-23 16:13:04 -0500

    Modified interactive plot to use a scatter plot instead of box plot for selection.

--- MitroAnalytics/interactivePlotScatter.R
+++ MitroAnalytics/interactivePlotScatter.R
@@ -43,8 +43,12 @@ ui <- fluidPage(
              
            )),
   fluidRow(
-           plotlyOutput("plot1")#,
-           #verbatimTextOutput("p1Select")
+    column(width = 2),
+    column(width = 8,
+           plotlyOutput("plot1"),
+           verbatimTextOutput("p1Select")),
+    
+    column(width = 2)
   ),
   
   fluidRow(
@@ -66,53 +70,69 @@ server <- function(input, output) {
   
   # Highlight selected rows in the scatterplot
   
+  ctr <- reactiveVal(0)
   m <- reactiveVal(NULL)
   d <- reactiveVal(NULL)
   
   observeEvent(input$elemChoice, {
     m <<- samp.elem[[input$elemChoice]] %>% tibble::remove_rownames()
     d <<- SharedData$new(m, ~solution_id)
-    
+    inc <- ctr() + 1
+    ctr(inc)
   })
   
-  # Drawing the plot and tracing the selected points.
   output$plot1 <- renderPlotly({
+    
+    #update <- input$elemChoice
     
     s <- input$data1_rows_selected
     
     if (!length(s)) {
       set.seed(1)      
       p <- d %>%
-        plot_ly(y = ~solid_conc, x = ~input$elemChoice, type = 'box', boxpoints = 'all', pointpos = 2, color = I('black'), name = 'Unfiltered', source = "p1_select") %>%
+        plot_ly(x = ~solid_conc, 
+          y = ~solution_id, 
+          type = 'scatter', 
+          color = I('black'), 
+          name = 'Unfiltered', 
+          source=ctr(), 
+          transforms = list(list(type='groupby',groups=input$elemChoice))) %>%
         highlight(on = "plotly_selected") %>%
-        layout(showlegend = T, boxgap = 0.5, dragmode = "select")
+        layout(showlegend = T, dragmode = "select")
       
     } else if (length(s)) {
       set.seed(1)
       pp <- d %>%
         plot_ly() %>% 
-        add_trace(y = ~solid_conc, x = ~input$elemChoice, type = 'box', boxpoints = 'all', pointpos = 2, color = I('black'), name = 'Unfiltered') %>%
+        add_trace(x = ~solid_conc, 
+          y = ~solution_id, 
+          type = "scatter", 
+          color = I('black'), 
+          name = 'Unfiltered',
+          source = ctr(),
+          transforms = list(list(type='groupby',groups=input$elemChoice))) %>%
         highlight(on = "plotly_selected") %>%
-        layout(showlegend = T, boxgap = 0.5, dragmode = "select")
+        layout(showlegend = T, dragmode = "select")
       
-      subplot(
-        pp,
-        select <- plot_ly(data = m[s, , drop = F], type = 'box', boxpoints = 'all', pointpos = 2, y = ~solid_conc, x = ~input$elemChoice, color = I('red'), name = 'Filtered') %>%
-          highlight(on= "plotly_selected")
-
-      )  %>% layout(showLegend = T, boxgap = 0.5) 
-
-
+      # selected data
+      set.seed(1)
+      pp <- add_trace(pp, 
+        data = m[s, , drop = F], 
+        type = 'scatter', 
+        x = ~solid_conc, 
+        y = ~solution_id, 
+        color = I('red'), 
+        name = 'Filtered',
+        transforms = list(list(type='groupby',groups=input$elemChoice))) %>%
+      layout(showLegend = T)
     }
   })
   
-  #### Examining Selected Data
-  
-  #output$p1Select <- renderPrint({
-  #  fromGraph <- event_data("plotly_selected") # Selection from the graph using built-in Plotly 'event_data()' function.
-  #  print("From Graph:")
-  #  fromGraph
-  #})
+  output$p1Select <- renderPrint({
+    fromGraph <<- event_data("plotly_selected",source=ctr()) # Selection from the graph using built-in Plotly 'event_data()' function.
+    print("From Graph:")
+    fromGraph
+  })
   
   output$crosstalk1 <- renderPrint({
     fromTable <- m[input$data1_rows_selected, ] # Selection from data table using 'crosstalk' package.
@@ -123,18 +143,29 @@ server <- function(input, output) {
   })
   
   output$data1 <- renderDT({
+    
+    m <- samp.elem[[input$elemChoice]] %>% tibble::remove_rownames()
+    
     m2 <- m[d$selection(),]
-    dt <- DT::datatable(m, option = list(pageLength = 50, rownames = FALSE)) # %>% DT::formatStyle('solution_id', target = 'row', valueColumns = , backgroundColor = 'red')
-  })
-  
-  
-  #### Event Monitoring
+    dt <- DT::datatable(d, option = list(pageLength = 50, rownames = FALSE))
+    if (NROW(m2) == 0) {
+      dt
+    } else {
+      DT::formatStyle(dt, "solution_id", target = "row",
+                      color = DT::styleEqual(m2$rowname, rep("white", length(m2$rowname))),
+                      backgroundColor = DT::styleEqual(m2$rowname, rep("black", length(m2$rowname))))
+    }
+  },server=FALSE)
   
   proxy = dataTableProxy('data1')
   
   observeEvent(d$selection(),{
+    #test <- event_data("plotly_selected")
+    #tabSelect <- test[[2]] + 1
+    #print(tabSelect)
+    #print(input$data1_rows_selected)
     tabSelect <- which(d$selection())
-    tabSelect <- rbind(tabSelect,input$data1_rows_selected)
+    #tabSelect <- rbind(tabSelect,input$data1_rows_selected)
     proxy %>% selectRows(tabSelect)
   })
   
@@ -143,6 +174,10 @@ server <- function(input, output) {
     if (is.null(test)) {
       proxy %>% selectRows(NULL)
     }
+  })
+  
+  observeEvent(ctr(),{
+    proxy %>% selectRows(NULL)
   })
 }
 
