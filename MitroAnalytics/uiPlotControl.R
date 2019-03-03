7205e6ed77ba2dd05942a8883a94cc0405f5bb3e
Christopher Eeles <christopher.eeles@outlook.com>
2019-03-02 23:51:11 -0500

    [Debugging] Added print statements
    * Removed some depricated code

--- MitroAnalytics/uiPlotControl.R
+++ MitroAnalytics/uiPlotControl.R
@@ -14,41 +14,56 @@ drawPlotlyPlot <- function(input, output, session, data, selectedElement) {
   # Initializing interactive data variables
   
   ctr <- reactiveVal(0) #Initializing reactive value to trigger datatable update when input$elemChoice is changed
-  m <- reactiveVal(NULL)
-  d <- reactiveVal(NULL)
+  m <- NULL
+  d <- NULL
   
   observeEvent(selectedElement, {
-    m <<- data %>% tibble::remove_rownames()
-    d <<- SharedData$new(m)
-    inc <- ctr() + 1
-    ctr(inc)
+    print("uiPlotControl.R - Initialize data")
+
+    dataTableProxy('data1') %>% selectRows(NULL)
+
+    m <<- data %>% tibble::rownames_to_column()
+    d <<- SharedData$new(m, ~solution_id)
   })
+
+  output$data1 <- renderDT({ # Renders the datatable
+
+    print("Rending dt")
+    dt <- DT::datatable(m, option = list(pageLength = 20))
+ 
+    }
+  },server=FALSE)
   
   output$plot1 <- renderPlotly({
     
-    s <- input$data1_rows_selected
+    s <- input$data1_rows_selected # Maybe if I make s a reactive value it will update when rows are deselected?
+
+    print("uiPlotControl.R - renderPlotly(plot1)")
     
     if (!length(s)) {
+      print("uiPlotControl.R - Render p")
       p <- d %>%
         plot_ly(x = ~solid_conc, 
                 y = ~solution_id, 
                 type = 'scatter', 
                 color = I('black'), 
                 name = 'Unfiltered',
-                source = "a",
+                mode = "markers",
                 transforms = list(list(type='groupby',groups=selectedElement))) %>%
         highlight(on = "plotly_selected",off="plotly_deselect") %>%
         layout(showlegend = T, dragmode = "select")
       
     } else if (length(s)) {
-      pp <- d %>%
+      print("uiPlotControl.R - Render pp")
+      c <- d
+      pp <- c %>%
         plot_ly() %>% 
         add_trace(x = ~solid_conc, 
                   y = ~solution_id, 
                   type = "scatter", 
                   color = I('black'), 
                   name = 'Unfiltered',
-                  source = "a",
+                  mode = "markers",
                   transforms = list(list(type='groupby',groups=selectedElement))) %>%
         highlight(on = "plotly_selected",off="plotly_deselect") %>%
         layout(showlegend = T, dragmode = "select")
@@ -57,22 +72,23 @@ drawPlotlyPlot <- function(input, output, session, data, selectedElement) {
       pp <- add_trace(pp, 
                       data = m[s, , drop = F], 
                       type = 'scatter', 
+                      mode = 'markers',
                       x = ~solid_conc, 
                       y = ~solution_id, 
                       color = I('red'), 
                       name = 'Filtered',
-                      transforms = list(list(type='groupby',groups=input$elemChoice))) %>%
-        layout(showLegend = T)
+                      transforms = list(list(type='groupby',groups=selectedElement)))
     }
   })
   
   # Renders interactive boxplot
   output$plot2 <- renderPlotly({
-
+    print("uiPlotControl.R - renderPlotly(plot2)")
     boxData <- m[input$data1_rows_selected, ] # Stores the datatable rows which are selected
 
     if(length(input$data1_rows_selected)){ # Renders when there are selected rows
-      b <- boxData %>%
+      print("uiPlotControl.R - Render bb")
+      bb <- boxData %>%
         plot_ly(x=~solid_conc,
                 type = "box",
                 boxpoints = "all",
@@ -81,8 +97,9 @@ drawPlotlyPlot <- function(input, output, session, data, selectedElement) {
                 pointpos = -1.5,
                 boxmean = TRUE
         )
-    } else if (!length(input$data1_rows_selectedm)) { # Renders when there aren't selected rows
-      bb <- d %>%
+    } else { # Renders when there aren't selected rows
+      print("uiPlotControl.R - Render b")
+      b <- d %>%
         plot_ly(x=~solid_conc,
                 type = "box",
                 color = I('black'),
@@ -96,7 +113,7 @@ drawPlotlyPlot <- function(input, output, session, data, selectedElement) {
   
   # Depricated: leave in as check condition in case of data selection problems
   output$p1Select <- renderPrint({
-    #fromGraph <<- event_data("ploty_selected", source = "a") # Selection from the graph using built-in Plotly 'event_data()' function.
+    #fromGraph <<- event_data("ploty_selected") # Selection from the graph using built-in Plotly 'event_data()' function.
     #print("From Graph:")
     #fromGraph
   })
@@ -111,36 +128,30 @@ drawPlotlyPlot <- function(input, output, session, data, selectedElement) {
     #input$data1_rows_selected
   })
   
-  output$data1 <- renderDT({ # Renders the datatable
-    
-    m2 <- m[d$selection(),]
-    dt <- DT::datatable(m, option = list(pageLength = 18, rownames = FALSE))
-    if (NROW(m2) == 0) {
-      dt
-    } else {
-      DT::formatStyle(dt, "solution_id", target = "row",
-                      color = DT::styleEqual(m2$rowname, rep("white", length(m2$rowname))),
-                      backgroundColor = DT::styleEqual(m2$rowname, rep("black", length(m2$rowname))))
-    }
-  })
-  
-  proxy = dataTableProxy('data1') # Allows update of datatable after initial rendering
-  
   observeEvent(d$selection(),{
+    print("uiPlotControl.R - observeEvent(selectRows(tabSelect)")
     tabSelect <- which(d$selection()) # Returns rows for which selection is true
-    print(tabSelect)
-    proxy %>% selectRows(tabSelect)
+    # print(tabSelect)
+    dataTableProxy('data1') %>% selectRows(tabSelect)
   })
   
-  observeEvent("plotly_selected",{ # Observes changes in plot selections
-    test <- event_data("plotly_selected",source = "a")
-    if (is.null(test)) {
-      proxy %>% selectRows(NULL)
-    }
-  })
+  #
+  # TESTING: Does this do anything important?
+  #
+  # observeEvent("plotly_selected",{ # Observes changes in plot selections
+  #   print("NULL selected rows by 'plotly_selected' event")
+  #   test <- event_data("plotly_selected")
+  #   if (is.null(test)) {
+  #     proxy %>% selectRows(NULL)
+  #   }
+  # })
   
-  observeEvent(ctr(),{ # Observes changes in input$elemChoice
-    proxy %>% selectRows(NULL)
-  })
+  #
+  # TESTING: Does this do anything important?
+  #
+  # observeEvent(ctr(),{ # Observes changes in input$elemChoice
+  #   print("NULL selected rows by ctr() event")
+  #   proxy %>% selectRows(NULL)
+  # })
 }
 
